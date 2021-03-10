27ed0a7d519413773381ea283248a86d761b1721
Vishal Bharadwaj <47346942+vbadwaj@users.noreply.github.com>
2021-03-10 22:00:47 +0400

    Create reduceToZero.java

---
+++ reduceToZero.java
@@ -0,0 +1,14 @@
+  
+class Solution {
+    public int numberOfSteps (int num) {
+        if(num==0)return 0;
+        int count=0;
+        while(num!=0){
+            if(num%2==0)num=num/2;
+            else num-=1;
+            count++;
+        }
+        return count;
+        
+    }
+}
