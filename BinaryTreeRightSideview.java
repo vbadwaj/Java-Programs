1df3f6e1eabd71d979adcabcd1f73a4e006b56bc
Vishal Bharadwaj <47346942+vbadwaj@users.noreply.github.com>
2021-05-14 15:01:00 +0400

    Create BinaryTreeRightSideview.java

---
+++ BinaryTreeRightSideview.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        Deque<TreeNode> q=new ArrayDeque<>();
+        List<Integer> sol=new ArrayList<>();
+        if(root==null)return sol;
+        q.add(root);
+        while(!q.isEmpty()){
+            int len=q.size();
+            for(int i=0;i<len;i++){
+                TreeNode curr=q.remove();
+                if(i==0){
+                    sol.add(curr.val);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+            }
+        }
+        return sol;
+    }
